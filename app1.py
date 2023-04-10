57b2af04d434ab558e0fb90801c34707ddf7d4ba
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-04-10 12:40:03 +0000

    finito tutto

--- app1.py
+++ app1.py
@@ -15,47 +15,47 @@ df=pd.read_csv("https://raw.githubusercontent.com/wtitze/3E/main/2010.csv", deli
 @app.route('/')
 def home():
     return render_template('home.html')
-
+#es1
 @app.route('/nome')
 def nome():
     return render_template('filmInserito.html')
-
+#es2
 @app.route('/genre')
 def genre():
     return render_template('genereNome.html')
-
+#es3
 @app.route('/tendina')
 def tendina():
     generi = df['Genres'].unique()
     #generi = df.drop_duplicates(subset=['Genres'])
     return render_template('genereTendina.html', list= list(generi))
-
+#es4
 @app.route('/radio')
 def radio():
     generi = df['Genres'].unique()
     #generi = df.drop_duplicates(subset=['Genres'])
     return render_template('Radio.html', list= list(generi))
-
+#es5
 @app.route('/check')
 def check():
     generi = df['Genres'].unique()
     #generi = df.drop_duplicates(subset=['Genres'])
     return render_template('Check.html', list= list(generi))
-
+#es6
 @app.route('/nan')
 def nan():
     df1=df[df["Budget"].isnull()]
     df2=df1.to_html()
     #generi = df.drop_duplicates(subset=['Genres'])
     return render_template('NAN.html', list= df2)
-
+#es7 variante 1
 @app.route('/graf')
 def graf():
     fig, ax= plt.subplots(figsize = (12,8))
     df.groupby("Genres")["Title"].count().sort_values(ascending=False).reset_index ().plot(kind="bar", ax=ax)
     graph = mpld3.fig_to_html(fig)#modo diverso dal solito
     return render_template('grafico.html',graf=graph)
-
+#es7 variante 2
 @app.route("/immagine")
 def immagine():
     fig, ax= plt.subplots(figsize = (12,8))
@@ -63,7 +63,7 @@ def immagine():
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)#metodo tradizionale usato in classe
     return Response(output.getvalue(), mimetype='image/png')
-
+#es1
 @app.route('/search_nome', methods = ['GET'])
 def search():
     film=request.args["nome"]
@@ -73,28 +73,28 @@ def search():
     else:
         dfhtm=mostro.to_html()
         return render_template('risultato.html', tabella = dfhtm)
-
+#es2
 @app.route('/search_genere', methods = ['GET'])
 def search_genere():
     film=request.args["genere"]
     genere=df[df["Genres"].str.lower().str.contains(film.strip().lower())]
     dfhtm=genere.to_html()
     return render_template('risultato.html', tabella = dfhtm)
-
+#es3
 @app.route('/search_tendina', methods = ['GET'])
 def search_tendina():
     film=request.args["genere"]
     genere=df[df["Genres"].str.lower().str.contains(film.strip().lower())]
     dfhtm=genere.to_html()
     return render_template('risultato.html', tabella = dfhtm)
-
+#es4
 @app.route('/search_Radio', methods = ['GET'])
 def search_radio():
     film=request.args["genere"]
     genere=df[df["Genres"].str.lower().str.contains(film.strip().lower())]
     dfhtm=genere.to_html()
     return render_template('risultato.html', tabella = dfhtm)
-
+#es5
 @app.route('/search_check', methods = ['GET'])
 def search_check():
     film=request.args.getlist("genere")
